6a9b815a33b044dff752dd8d1c5dcc0719b000d3
Nico <jaylordiii@gmail.com>
2021-02-13 17:20:23 +0100

    RayCaster Component

---
+++ C_RayCaster.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nico on 13.02.2021.
+//
+
+#include "C_RayCaster.h"
---
+++ C_RayCaster.h
@@ -0,0 +1,15 @@
+//
+// Created by Nico on 13.02.2021.
+//
+
+#ifndef INC_2DENGINE_C_RAYCASTER_H
+#define INC_2DENGINE_C_RAYCASTER_H
+
+#include "Component.h"
+
+class C_RayCaster :public Component {
+
+};
+
+
+#endif //INC_2DENGINE_C_RAYCASTER_H
